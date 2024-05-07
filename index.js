de9b2c9c3f37eb89af777139f0e4bf4d63cbd7dc
gchgfgjh <jsjcames24lloyd@hotmail.com>
2024-05-08 05:15:48 +0700

    start project

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-function rightSideView(root) {
+function levelOrderBottom(root) {
   if (!root) return [];
   const result = [];
-  let level = 0;
-  const traverse = (node, level) => {
-    if (!node) return;
-    if (result[level] === undefined) result[level] = node.val;
-    traverse(node.right, level + 1);
-    traverse(node.left, level + 1);
-  };
-  traverse(root, level);
+  const queue = [root];
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.unshift(currentLevel);
+  }
   return result;
 }
